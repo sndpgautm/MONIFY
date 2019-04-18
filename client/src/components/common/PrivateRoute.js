4681c2477c6770b6872de6c32dc598b6ed48981b
Sandip Gautam <rockersandip96@gmail.com>
2019-04-19 01:43:57 +0300

    Added PrivateRoute component for react app
    Created a PrivateRoute component to restrict anuthorized users from gaining access to protected routes like dashboard page which contains sensitive expense informations

---
+++ client/src/components/common/PrivateRoute.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+// Checks to see if user is authenticated and loads the passed Component if its true or redirects to Login if its false
+const PrivateRoute = ({ component: Component, auth, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      auth.isAuthenticated === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
